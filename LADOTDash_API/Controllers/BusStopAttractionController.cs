ba668aa02da4d49e95803dafe677a59161bedb24
Gia <gianina.oui@gmail.com>
2018-05-31 09:56:43 -0700

    ''

--- LADOTDash_API/Controllers/BusStopAttractionController.cs
+++ LADOTDash_API/Controllers/BusStopAttractionController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LADOTDash_API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,26 @@ namespace LADOTDash_API.Controllers
 {
     public class BusStopAttractionController : ApiController
     {
+
+        LADOTDashDB _db = new LADOTDashDB();
+
+
+
+        public IEnumerable<Attraction> GetAllAttractions()
+        {
+            var attractions = _db.Attractions.ToList();
+            return attractions;
+        }
+
+        public IHttpActionResult GetAttraction(int id)
+        {
+            var attractions = _db.Attractions.ToList();
+            var attraction = attractions.FirstOrDefault((p) => p.AttractionID == id);
+            if (attraction == null)
+            {
+                return NotFound();
+            }
+            return Ok(attraction);
+        }
     }
 }
